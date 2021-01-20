d340bc6c7348b8a59a25a30819bcec509c659bca
Jeremy Trpka <jeremy.trpka@perficient.com>
2021-01-20 15:24:31 -0500

    Lesson 06 Confirm Reduced

--- backstop_data/engine_scripts/puppet/custom/clickConfirm.js
+++ backstop_data/engine_scripts/puppet/custom/clickConfirm.js
@@ -1,7 +1,6 @@
+const fillOutRequiredFields = require('./fillOutRequiredFields');
+
 module.exports = async page => {
-    await page.type("input#first-name", "Jeremy");
-    await page.type("input#last-name", "Trpka");
-    await page.type("input#email", "atlantis1982@gmail.com");
-    await page.click("input#agree");
+    await fillOutRequiredFields(page);
     await page.click('button[type="submit"]');
 }
